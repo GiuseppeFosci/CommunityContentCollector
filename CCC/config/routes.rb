d560a6bb49433bf5d6a6cc27ee9ad46e911cf9ff
erem1x <martino.1715650@studenti.uniroma1.it>
2022-12-07 00:03:17 +0100

    Added title helper

--- CCC/config/routes.rb
+++ CCC/config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
+  root "static_pages#home"
   get 'static_pages/home'
   get 'static_pages/help'
   resources :microposts
   resources :users
-  root 'users#index'
   
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
