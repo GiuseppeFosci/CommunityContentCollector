e4c7557a932084afe94e4e0ea10c4fd87dda35e0
erem1x <martino.1715650@studenti.uniroma1.it>
2022-12-08 19:20:14 +0100

    Add password

--- CCC/app/models/user.rb
+++ CCC/app/models/user.rb
@@ -4,4 +4,9 @@ class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A([a-z]+(.)\d{6,7}(@studenti.uniroma1.it))|([a-z]+(.)[a-z]+(@uniroma1.it))\z/i
 
   validates :email, presence: true, length: {maximum: 255}, format: { with: VALID_EMAIL_REGEX }, uniqueness: true
+  
+  #bcrypt password sicura
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }
+
 end
