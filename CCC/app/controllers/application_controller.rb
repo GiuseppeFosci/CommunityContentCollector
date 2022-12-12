aa9fb5d882481d22d9ae82a836532506e09d1d41
GiuseppeFosci <fosci.1855832@studenti.uniroma1.it>
2022-12-13 00:07:31 +0100

    Implementazione login base

--- CCC/app/controllers/application_controller.rb
+++ CCC/app/controllers/application_controller.rb
@@ -1,6 +1,13 @@
 class ApplicationController < ActionController::Base
+    include SessionsHelper
+    
     def hello
         render html: "hello, world!"
+    end
+
+    # Logs in the given user.
+    def log_in(user)
+        session[:user_id] = user.id
     end
 
 
