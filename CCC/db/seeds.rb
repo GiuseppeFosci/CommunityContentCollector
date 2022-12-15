2d84d7dbb79c5439c3465afbf22d240525bae909
Arx2399 <alessiorago99@gmail.com>
2022-12-15 23:48:01 +0100

    Implementazione impaginazione utenti

--- CCC/db/seeds.rb
+++ CCC/db/seeds.rb
@@ -1,7 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+# Create a main sample user.
+User.create!(name: "Example", surname: "User", email: "example.user@uniroma1.it", password: "foobar", password_confirmation: "foobar")
+    
+# Generate a bunch of additional users.
+99.times do |n|
+    name = Faker::Name.first_name
+    surname = Faker::Name.last_name
+    email = "example.12345#{n+1}@studenti.uniroma1.it"
+    password = "password"
+    User.create!(name: name, surname: surname, email: email, password: password, password_confirmation: password)
+end
