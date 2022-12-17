7ea6d4ba75ddc256823f1c2b33bb77ac83bca662
erem1x <martino.1715650@studenti.uniroma1.it>
2022-12-17 16:36:45 +0100

    Test attivazione account

--- CCC/app/controllers/account_activations_controller.rb
+++ CCC/app/controllers/account_activations_controller.rb
@@ -1,2 +1,17 @@
 class AccountActivationsController < ApplicationController
+
+    def edit
+        user = User.find_by(email: params[:email])
+        if user && !user.activated? && user.authenticated?(:activation, params[:id])
+            user.update_attribute(:activated, true)
+            user.update_attribute(:activated_at, Time.zone.now)
+            log_in user
+            flash[:success] = "Account attivato!"
+            redirect_to user
+        else
+            flash[:danger] = "Link di attivazione non valido"
+            redirect_to root_url
+        end
+    end
 end
+
