e3556e5966727f4d2fb0dfe0f3d57d89da82f435
Arx2399 <alessiorago99@gmail.com>
2022-12-20 22:32:35 +0100

    Implementazione ordine post

--- CCC/test/models/post_test.rb
+++ CCC/test/models/post_test.rb
@@ -26,4 +26,8 @@ class PostTest < ActiveSupport::TestCase
     @post.content = "a" * 10001
     assert_not @post.valid?
   end
+
+  test "order should be most recent first" do
+    assert_equal posts(:most_recent), Post.first
+  end
 end
