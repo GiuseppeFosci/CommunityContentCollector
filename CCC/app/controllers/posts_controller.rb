a02d3c3b653d11c6f9f8c05f681173b1bfc56240
GiuseppeFosci <fosci.1855832@studenti.uniroma1.it>
2022-12-21 23:28:42 +0100

    Bugs fix

--- CCC/app/controllers/posts_controller.rb
+++ CCC/app/controllers/posts_controller.rb
@@ -2,8 +2,8 @@ class PostsController < ApplicationController
     before_action :logged_in_user, only: [:create, :destroy]
     before_action :correct_user,only: :destroy
     def create
-        @micropost = current_user.microposts.build(micropost_params)
-        if @micropost.save
+        @post = current_user.posts.build(post_params)
+        if @post.save
             flash[:success] = "Micropost created!"
             redirect_to root_url
         else
