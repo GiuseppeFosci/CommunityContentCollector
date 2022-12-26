5e089e1324e7dfe7e9ed4f5c75348a6cb3147ae9
Arx2399 <alessiorago99@gmail.com>
2022-12-26 17:56:32 +0100

    Implementazione test utenti seguiti e seguaci

--- CCC/test/models/user_test.rb
+++ CCC/test/models/user_test.rb
@@ -81,6 +81,7 @@ test "should follow and unfollow a user" do
   assert_not michael.following?(archer)
   michael.follow(archer)
   assert michael.following?(archer)
+  assert archer.followers.include?(michael)
   michael.unfollow(archer)
   assert_not michael.following?(archer)
   # Users can't follow themselves.
