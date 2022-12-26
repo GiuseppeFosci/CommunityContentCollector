578584478d834c1bc8ffc649ac2ad269dbe789ca
Arx2399 <alessiorago99@gmail.com>
2022-12-26 17:49:45 +0100

    Implementazione funzioni seguiti e non più seguiti

--- CCC/test/models/user_test.rb
+++ CCC/test/models/user_test.rb
@@ -75,4 +75,17 @@ test "associated posts should be destroyed" do
   end
 end
 
+test "should follow and unfollow a user" do
+  michael = users(:michael)
+  archer = users(:archer)
+  assert_not michael.following?(archer)
+  michael.follow(archer)
+  assert michael.following?(archer)
+  michael.unfollow(archer)
+  assert_not michael.following?(archer)
+  # Users can't follow themselves.
+  michael.follow(michael)
+  assert_not michael.following?(michael)
+  end
+
 end
