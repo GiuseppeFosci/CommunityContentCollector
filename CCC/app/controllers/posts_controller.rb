0b2016b7df9c486c1887221b09835a30a5237dce
erem1x <martino.1715650@studenti.uniroma1.it>
2022-12-30 18:33:10 +0100

    Vista admin

--- CCC/app/controllers/posts_controller.rb
+++ CCC/app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
     before_action :logged_in_user, only: [:create, :destroy]
-    before_action :correct_user,only: :destroy
+    before_action :correct_user, only: :destroy
+    
     def create
         @post = current_user.posts.build(post_params)
 
@@ -15,7 +16,7 @@ class PostsController < ApplicationController
     end
     
     def destroy
-        @post.destroy
+        post.find(params[:id]).destroy
         flash[:success] = "Post eliminato"   
         if request.referrer.nil?
             redirect_to root_url, status: :see_other
@@ -30,11 +31,10 @@ class PostsController < ApplicationController
         params.require(:post).permit(:content, :file => [])
     end
 
+
     def correct_user
-        @post = current_user.posts.find_by(id: params[:id])
-        redirect_to root_url, status: :see_other if @post.nil?
+      redirect_to(root_url, status: :see_other) unless
+      current_user.admin?
     end
-
-
 
 end
