e6d8431d54da966cfc1ee4b4205968aeeacba108
erem1x <martino.1715650@studenti.uniroma1.it>
2023-01-06 01:04:58 +0100

    Added other categories

--- CCC/db/seeds.rb
+++ CCC/db/seeds.rb
@@ -15,7 +15,7 @@ users = User.order(:created_at).take(6)
 
 50.times do
     content = Faker::Lorem.sentence(word_count: 5)
-    category = "Ingegneria"
+    category = ["Archeologia", "Biologia", "Economia", "Ingegneria", "Lettere", "Lingue", "Medicina"].sample
     users.each { |user| user.posts.create!(content: content, category: category) }
 end
 
