d69c334357f9805bb695c57e9e5ade8e05a53302
erem1x <martino.1715650@studenti.uniroma1.it>
2023-01-12 14:42:22 +0100

    fixing rspec post model test

--- CCC/spec/models/post_rspec.rb
+++ CCC/spec/models/post_rspec.rb
@@ -4,35 +4,28 @@ RSpec.describe Post, type: :model do
     
     before(:all) do
         @user1 = create(:user)
-        @post1 = create(:post)
     end
     
     describe "validations" do
         
-        it "should be valid" do
-            user= users(:michael)
-            post= user.posts.build(content: "Test di post", category: "Economia")
+        it "post should be valid" do
+            post = @user1.posts.build(:post)
             assert post.valid?
         end
     
         it "user id should be present" do
-            user = users(:michael)
-            post = user.posts.build(content: "Test di post", category: "Economia")
+            post = @user1.posts.build(:post)
             post.user_id = nil
             expect(post).to_not be_valid
         end
         
         it "content should be present" do
-            user = users(:michael)
-            post = user.posts.build(content: "Test di post", category: "Economia")
-            post.content = " "
+            post = @user1.posts.build(:post, content: " ")
             expect(post).to_not be_valid
         end
         
         it "content should be at most 10000 characters" do
-            user = users(:michael)
-            post = user.posts.build(content: "Test di post", category: "Economia")
-            post.content = "a" * 10001
+            post = @user1.posts.build(:post, content: "a" * 10001)
             expect(post).to_not be_valid
         end
         
